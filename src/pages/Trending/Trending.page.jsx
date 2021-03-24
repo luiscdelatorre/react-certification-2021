03f396982f563147f124913595c23fbb639ceb03
Luis Carlos de la Torre <luis.delatorre@wizeline.com>
2021-03-23 20:42:22 -0600

    Add new Trending Videos Page

---
+++ src/pages/Trending/Trending.page.jsx
@@ -0,0 +1,69 @@
+import React, { useEffect, useState } from 'react';
+import { YoutubeApi } from '../../api/youtube.api';
+import VideoCard from '../../components/VideoCard';
+import { formatDate } from '../../utils/fns';
+import Styled from '../Home/Home.styles';
+
+const TrendingPage = () => {
+  const [trendingVideos, setTrendingVideos] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = () => {
+      YoutubeApi.trending()
+        .then((result) => {
+          setTrendingVideos(result?.items);
+        })
+        .catch(() => {
+          setError(
+            `The request cannot be completed because you have exceeded your quota.
+            Please try again later.`
+          );
+        });
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <Styled.Container>
+      <Styled.Hero>
+        <h2>Most popular videos</h2>
+      </Styled.Hero>
+      <hr />
+      {error ? (
+        <Styled.Error>{error}</Styled.Error>
+      ) : (
+        <Styled.CardList data-testid="search-list">
+          {trendingVideos.map(
+            ({
+              etag,
+              id,
+              snippet: {
+                title,
+                channelTitle,
+                publishedAt,
+                thumbnails,
+                liveBroadcastContent,
+              },
+            }) => {
+              return (
+                <Styled.CardListItem key={etag}>
+                  <VideoCard
+                    id={id.videoId || id}
+                    title={title}
+                    channel={channelTitle}
+                    date={formatDate(publishedAt)}
+                    thumbnail={thumbnails.medium.url}
+                    liveBroadcastContent={liveBroadcastContent}
+                  />
+                </Styled.CardListItem>
+              );
+            }
+          )}
+        </Styled.CardList>
+      )}
+    </Styled.Container>
+  );
+};
+
+export default TrendingPage;
---
+++ src/pages/Trending/Trending.test.jsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import { screen, render, act } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import axios from 'axios';
+import AuthProvider from '../../providers/Auth';
+import SearchProvider from '../../providers/Search';
+import trending from '../../mock/youtube-trending-mock.json';
+import SessionDataProvider from '../../providers/SessionData/SessionData.provider';
+import TrendingPage from './Trending.page';
+
+jest.mock('axios');
+
+describe('Trending Page', () => {
+  beforeEach(() => {
+    const response = { data: trending };
+    axios.get.mockResolvedValue(response);
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+    axios.mockRestore();
+  });
+
+  it('Should display login option if user is not authenticated', async () => {
+    await act(async () => {
+      render(
+        <BrowserRouter>
+          <AuthProvider>
+            <SessionDataProvider>
+              <SearchProvider>
+                <TrendingPage />
+              </SearchProvider>
+            </SessionDataProvider>
+          </AuthProvider>
+        </BrowserRouter>
+      );
+    });
+
+    expect(screen.queryByTestId('search-list').children.length).toBe(
+      trending.items.length
+    );
+    expect(screen.queryByText('Most popular videos')).toBeInTheDocument();
+  });
+
+  it('Should display quota error', async () => {
+    axios.get.mockRejectedValue({});
+
+    await act(async () => {
+      render(
+        <BrowserRouter>
+          <AuthProvider>
+            <SessionDataProvider>
+              <SearchProvider>
+                <TrendingPage />
+              </SearchProvider>
+            </SessionDataProvider>
+          </AuthProvider>
+        </BrowserRouter>
+      );
+    });
+
+    expect(
+      screen.queryByText(
+        'The request cannot be completed because you have exceeded your quota. Please try again later.'
+      )
+    ).toBeInTheDocument();
+  });
+});
