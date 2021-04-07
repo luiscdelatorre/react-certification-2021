cd7006dc1acad65de90d4c2fb09649825f16aca0
Luis Carlos de la Torre <luis.delatorre@wizeline.com>
2021-04-07 00:17:40 -0500

    Update login tests

--- src/api/login.test.js
+++ src/api/login.test.js
@@ -1,14 +1,16 @@
 import { loginApi } from './login.api';
 
-describe('/Login Api', () => {
-  it('should throw an error with wrong credentials', async () => {
-    await loginApi('foo', 'bar').catch((e) => {
-      expect(e.message).toBe('Username or password invalid');
-    });
+describe('Login Api', () => {
+  it('should throw an error with wrong credentials', () => {
+    expect(() => loginApi('foo', 'Rocks!').toThrow('Username or password invalid'));
   });
-  it('should return user data', async () => {
-    await loginApi('wizeline', 'Rocks!').then((user) => {
-      expect(user.name).toBe('Wizeline');
-    });
+
+  it('should return user data', (done) => {
+    loginApi('wizeline', 'Rocks!')
+      .then((user) => {
+        expect(user.name).toBe('Wizeline');
+        done();
+      })
+      .catch(done);
   });
 });
